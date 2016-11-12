a0d8f4bf1c26819846af67d6bfe35d7d27f0cb2b
Ben Phelps <ben@phelps.io>
2016-11-11 22:42:20 -0600

    mangos auth provider

--- config/app.php
+++ config/app.php
@@ -178,6 +178,8 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
+        App\Providers\MangosAuthServiceProvider::class,
+        App\Providers\MangosUserProvider::class,
 
     ],
 
