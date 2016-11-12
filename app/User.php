761d643565b1559721dff958e6226b6a555d9f65
Brandon Jank <jank6275@vandals.uidaho.edu>
2016-11-11 18:30:10 -0800

    fix unique

--- app/User.php
+++ app/User.php
@@ -11,6 +11,7 @@ class User extends Authenticatable
 
     protected $connection = 'realmd';
     protected $table = 'account';
+    public $timestamps = false;
 
     /**
      * The attributes that are mass assignable.
