ba2582da24bb7a6b11f56e71c6da8c983d1b3c1f
Brandon Jank <jank6275@vandals.uidaho.edu>
2016-11-12 01:08:49 -0800

    accounts page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -59,21 +59,21 @@
                         @else
                             <li><a href="{{ url('/realms') }}">Realms</a></li>
                             <li class="dropdown">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
                                     Accounts <span class="caret"></span>
                                 </a>
-                                <ul class="dropdown-menu" role="menu">
+                                <ul class="dropdown-menu">
                                     <li><a href="{{ url('/accounts') }}">List</a></li>
                                     <li><a href="{{ url('/accounts/create') }}">Create</a></li>
                                 </ul>
                             </li>
                             <li><a href="{{ url('/characters') }}">Characters</a></li>
                             <li class="dropdown">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
                                     {{ Auth::user()->name }} <span class="caret"></span>
                                 </a>
 
-                                <ul class="dropdown-menu" role="menu">
+                                <ul class="dropdown-menu">
                                     <li>
                                         <a href="{{ url('/logout') }}"
                                             onclick="event.preventDefault();
