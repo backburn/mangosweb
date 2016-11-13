3e4ad1c20b363eb71f19639a1b7383322f67d871
Ben Phelps <ben@phelps.io>
2016-11-13 00:47:21 -0600

    lets try

---
+++ app/Http/Helpers.php
@@ -0,0 +1,59 @@
+<?php
+
+function race_image($race) {
+    return '/img/' . [
+        1 => "human",
+        2 => "orc",
+        3 => "dwarf",
+        4 => "nightelf",
+        5 => "undead",
+        6 => "tauren",
+        7 => "hnome",
+        8 => "troll",
+        10 => "bloodelf",
+        11 => "draenei"
+    ][$race] . '.png' ?? '';
+}
+
+function class_image($class) {
+    return 'img' . [
+        1 => "warrior",
+        2 => "paladin",
+        3 => "hunter",
+        4 => "rogue",
+        5 => "priest",
+        7 => "shaman",
+        8 => "nage",
+        9 => "warlock",
+        11 => "druid"
+    ][$class] . '.png' ?? '';
+}
+
+function race_name($race) {
+    return [
+        1 => "Human",
+        2 => "Orc",
+        3 => "Dwarf",
+        4 => "Night Elf",
+        5 => "Undead",
+        6 => "Tauren",
+        7 => "Gnome",
+        8 => "Troll",
+        10 => "Blood Elf",
+        11 => "Draenei"
+    ][$race] ?? '';
+}
+
+function class_name($class) {
+    return [
+        1 => "Warrior",
+        2 => "Paladin",
+        3 => "Hunter",
+        4 => "Rogue",
+        5 => "Priest",
+        7 => "Shaman",
+        8 => "Mage",
+        9 => "Warlock",
+        11 => "Druid"
+    ][$class] ?? '';
+}
