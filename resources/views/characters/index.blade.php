5cec39e083e8aedd1ec068966c64e222d019d56a
Ben Phelps <ben@phelps.io>
2016-11-13 01:22:52 -0600

    lets try

--- resources/views/characters/index.blade.php
+++ resources/views/characters/index.blade.php
@@ -4,28 +4,30 @@
 <div class="container">
     @foreach($accounts as $account)
     <div class="row">
-        <div class="col-xs-12 col-md-10 col-md-offset-1">
+        <div class="col-xs-12">
             <div class="panel panel-default">
                 <div class="panel-heading">{{ $account->username }}</div>
                 <div class="panel-body">
-                    @foreach ($account->characters as $character)
-                        <div class="panel panel-primary">
-                            <div class="panel-heading">
-                                <span class="pull-left">{{ $character->name }}</span>
-                                <span class="pull-right">
+                    <div class="row">
+                        @foreach ($account->characters as $character)
+                            <div class="panel panel-primary col-xs-12 col-sm-12 col-md-4">
+                                <div class="panel-heading">
+                                    <span class="pull-left">{{ $character->name }}</span>
+                                    <span class="pull-right">
                                     <img width="24" src="{{ race_image($character->gender, $character->race) }}">
                                     <img width="24" src="{{ class_image($character->class) }}">
                                 </span>
+                                </div>
+                                <div class="panel-body">
+                                    Panel content
+                                    <dl class="dl-horizontal">
+                                        <dt>Gold</dt>
+                                        <dd>{{ money($character->money) }}</dd>
+                                    </dl>
+                                </div>
                             </div>
-                            <div class="panel-body">
-                                Panel content
-                                <dl class="dl-horizontal">
-                                    <dt>Gold</dt>
-                                    <dd>{{ money($character->money) }}</dd>
-                                </dl>
-                            </div>
-                        </div>
-                    @endforeach
+                        @endforeach
+                    </div>
                 </div>
             </div>
         </div>
