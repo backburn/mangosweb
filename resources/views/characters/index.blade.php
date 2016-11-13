0efd1e5a77fabaf399b415f5917e0776da1a13e1
Ben Phelps <ben@phelps.io>
2016-11-13 01:25:45 -0600

    lets try

--- resources/views/characters/index.blade.php
+++ resources/views/characters/index.blade.php
@@ -10,20 +10,22 @@
                 <div class="panel-body">
                     <div class="row">
                         @foreach ($account->characters as $character)
-                            <div class="panel panel-primary col-xs-12 col-sm-12 col-md-4">
-                                <div class="panel-heading">
-                                    <span class="pull-left">{{ $character->name }}</span>
-                                    <span class="pull-right">
+                            <div class="col-xs-12 col-sm-12 col-md-4">
+                                <div class="panel panel-primary">
+                                    <div class="panel-heading">
+                                        <span class="pull-left">{{ $character->name }}</span>
+                                        <span class="pull-right">
                                     <img width="24" src="{{ race_image($character->gender, $character->race) }}">
                                     <img width="24" src="{{ class_image($character->class) }}">
                                 </span>
-                                </div>
-                                <div class="panel-body">
-                                    Panel content
-                                    <dl class="dl-horizontal">
-                                        <dt>Gold</dt>
-                                        <dd>{{ money($character->money) }}</dd>
-                                    </dl>
+                                    </div>
+                                    <div class="panel-body">
+                                        Panel content
+                                        <dl class="dl-horizontal">
+                                            <dt>Gold</dt>
+                                            <dd>{{ money($character->money) }}</dd>
+                                        </dl>
+                                    </div>
                                 </div>
                             </div>
                         @endforeach
